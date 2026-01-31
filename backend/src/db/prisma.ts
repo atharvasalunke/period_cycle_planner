371f7cda9aaf769e02e657a65d45884e369fb192
atharvasalunke <atharvasalunke14@gmail.com>
2026-01-31 18:22:01 -0500

    fixed prisma issue

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  adapter: process.env.DATABASE_URL,
-});
+const prisma = new PrismaClient();
 
 export { prisma };
