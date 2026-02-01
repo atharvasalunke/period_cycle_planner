17b012e30d5d3ba1d863e0048d7c6834f857a227
ForgottenLight4415 <crv4415@gmail.com>
2026-02-01 01:03:33 -0500

    Implement Google Tasks integration with create and delete functionality; add date parsing utility

---
+++ src/lib/date.ts
@@ -0,0 +1,8 @@
+export const parseDateInput = (value: string): Date => {
+  if (/^\d{4}-\d{2}-\d{2}$/.test(value)) {
+    const [year, month, day] = value.split('-').map(Number);
+    return new Date(year, month - 1, day);
+  }
+
+  return new Date(value);
+};
